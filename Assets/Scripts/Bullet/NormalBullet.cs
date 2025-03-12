9d6a5cc661d57eec0a57460adfbc31eafc27d925
DESKTOP-S8U5MUK\DELL <2421432410@qq.com>
2025-03-12 22:17:49 +0800

    啥都没改

--- Assets/Scripts/Bullet/NormalBullet.cs
+++ Assets/Scripts/Bullet/NormalBullet.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class NormalBullet : BulletBase
 {
     protected float bulletSpeed;
-    
+    private BoxCollider2D boxCollider;
     protected override void Shoot()
     {
         gameObject.transform.localPosition += Time.deltaTime * bulletSpeed * transform.up;
